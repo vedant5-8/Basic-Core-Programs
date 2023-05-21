d5eb9fb9b184356eb1565e5f9edd63894860ec12
Vedant <vedantnp200@gmail.com>
2023-05-22 01:00:51 +0530

    Write flip coin program and leap year program

---
+++ FlipCoin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Programs
+{
+    public class FlipCoin
+    {
+        public static void PercentageOfCoinFlip()
+        {
+            Random random = new Random();
+            Console.WriteLine("Write how many times you want to flip the coin: ");
+            int numberOfFlips = Convert.ToInt32(Console.ReadLine());
+
+            int head = 0;
+            int tail = 0;
+
+            if (numberOfFlips > 0)
+            {
+                for(int i = 1; i <= numberOfFlips; i++)
+                {
+                    int randomNumber = random.Next(0, 2);
+
+                    if (randomNumber == 0)
+                    {
+                        head++;
+                    }
+                    else
+                    {
+                        tail++;
+                    }
+                }
+
+                double PercentageOfHeads = head / numberOfFlips * 100;
+                double PercentageOfTails = tail / numberOfFlips * 100;
+
+                Console.WriteLine("Percentage of Heads: {0}%", PercentageOfHeads);
+                Console.WriteLine("Percentage of Tails: {0}%", PercentageOfTails);
+            }
+            else
+            {
+                Console.WriteLine("You have entered wrong value.");
+                Console.WriteLine("Next time enter positive value.");
+                Environment.Exit(0);
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using Basic_Core_Programs;
+using System;
+
+namespace BasicCorePrograms
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            
+            while (true)
+            {
+                Console.WriteLine("Choose a number: ");
+                Console.WriteLine("1. Flip Coin and print percentage of Heads and Tails");
+                Console.WriteLine("2. Leap Year");
+                Console.WriteLine("3. Power of 2");
+                Console.WriteLine("4. Exit.");
+                Console.Write("=>");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        FlipCoin.PercentageOfCoinFlip();
+                        break;
+                    case 2: 
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Enter correct choice.");
+                        break;
+                }
+            }
+
+        }
+    }
+}
